3c18b525c6c016e7612a098a37b314b1c0e63995
Matthew Kent <matthew.e.kent@gmail.com>
2013-03-14 17:55:01 -0700

    move cards without reloading activity; move temp nav buttons up top

--- src/com/kumquatcards/ui/FlashCardActivity.java
+++ src/com/kumquatcards/ui/FlashCardActivity.java
@@ -3,7 +3,6 @@ package com.kumquatcards.ui;
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
-import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -20,11 +19,17 @@ import com.kumquatcards.provider.HskContract;
 public class FlashCardActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
 	public static final String TAG = "FlashCardActivity";
 
-	private String simplified;
+	private String currentTranslation;
+	private int currentLevel;
+	private int currentOrder;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		currentLevel = Integer.parseInt(getIntent().getData().getPathSegments().get(1));
+		currentOrder = Integer.parseInt(getIntent().getData().getPathSegments().get(2));
+
 		setContentView(R.layout.activity_flash_card);
 		getLoaderManager().initLoader(0, null, this);
 	}
@@ -33,45 +38,36 @@ public class FlashCardActivity extends Activity implements LoaderManager.LoaderC
 		EditText translationText = (EditText) findViewById(R.id.card_translation);
 		String input = translationText.getText().toString();
 		
-		String message = input.equalsIgnoreCase(simplified) ? "correct!" : "wrong :(";
+		String message = input.equalsIgnoreCase(currentTranslation) ? "correct!" : "wrong :(";
 		Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
 		toast.show();
 	}
 
 	public void movePrev(View view) {
-		int level = Integer.parseInt(getIntent().getData().getPathSegments().get(1));
-		int order = Integer.parseInt(getIntent().getData().getPathSegments().get(2));
-		if (order > 1) {
-			order -= 1;
+		if (currentOrder > 1) {
+			currentOrder -= 1;
 		}
-		Uri uri = HskContract.FlashCards.buildFlashCardUri(level, order);
-
-		Intent intent = new Intent(Intent.ACTION_VIEW, uri, this, FlashCardActivity.class);
-		startActivity(intent);
+		getLoaderManager().restartLoader(0, null, this);
 	}
 
 	public void moveNext(View view) {
-		int level = Integer.parseInt(getIntent().getData().getPathSegments().get(1));
-		int order = Integer.parseInt(getIntent().getData().getPathSegments().get(2));
-		if (order < HskContract.FlashCards.maxOrderForLevel(level)) {
-			order += 1;
+		if (currentOrder < HskContract.FlashCards.maxOrderForLevel(currentLevel)) {
+			currentOrder += 1;
 		}
-		Uri uri = HskContract.FlashCards.buildFlashCardUri(level, order);
-
-		Intent intent = new Intent(Intent.ACTION_VIEW, uri, this, FlashCardActivity.class);
-		startActivity(intent);
+		getLoaderManager().restartLoader(0, null, this);
 	}
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		return new CursorLoader(this, getIntent().getData(), null, null, null, null);
+		Uri uri = HskContract.FlashCards.buildFlashCardUri(currentLevel, currentOrder);
+		return new CursorLoader(this, uri, null, null, null, null);
 	}
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
 		TextView card = (TextView) findViewById(R.id.card_text);
 		if (cursor.moveToFirst()) {
-			simplified = new String(cursor.getBlob(cursor.getColumnIndex(HskContract.FlashCards.COLUMN_NAME_SIMPLIFIED)));
+			currentTranslation = new String(cursor.getBlob(cursor.getColumnIndex(HskContract.FlashCards.COLUMN_NAME_SIMPLIFIED)));
 			String text = new String(cursor.getBlob(cursor.getColumnIndex(HskContract.FlashCards.COLUMN_NAME_DEFINITION)));
 			card.setText(text);
 		} else {
